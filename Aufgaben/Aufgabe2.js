9c598b7dfde71d3ac612a94e9bd1abc76d003707
Timo 8litzCode <29957106+Txpert@users.noreply.github.com>
2024-03-26 10:31:25 +0100

    Create Aufgabe2.js

---
+++ Aufgaben/Aufgabe2.js
@@ -0,0 +1,6 @@
+const buecher = [
+  { titel: "Der Hobbit", autor: "J.R.R. Tolkien", jahr: 1937, gelesen: true },
+  { titel: "Harry Potter und der Stein der Weisen", autor: "J.K. Rowling", jahr: 1997, gelesen: false },
+  { titel: "1984", autor: "George Orwell", jahr: 1949, gelesen: true },
+  { titel: "Brave New World", autor: "Aldous Huxley", jahr: 1932, gelesen: false }
+];
