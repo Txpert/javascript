ba751e26a82e8eaee83c3e3e47be238022853356
Txpert <29957106+Txpert@users.noreply.github.com>
2024-03-26 03:58:56 +0100

    First commit

---
+++ Aufgaben/Aufgabe1.js
@@ -0,0 +1,8 @@
+const inventar = [
+  { name: "Apfel", kategorie: "Obst", anzahl: 10 },
+  { name: "Banane", kategorie: "Obst", anzahl: 20 },
+  { name: "Birne", kategorie: "Obst", anzahl: 15 },
+  { name: "Karotte", kategorie: "Gemüse", anzahl: 10 },
+  { name: "Spinat", kategorie: "Gemüse", anzahl: 5 },
+  { name: "Kartoffel", kategorie: "Gemüse", anzahl: 25 }
+];
